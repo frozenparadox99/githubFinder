38d93b1c81fa8360de8fb28b91df5b88de00ac4d
frozenparadox99 <frostbitecryengine300@gmail.com>
2020-03-16 16:52:47 +0530

    added repos subsection

--- src/App.js
+++ src/App.js
@@ -16,7 +16,8 @@ class App extends Component {
     users: [],
     user: {},
     loading: false,
-    alert: null
+    alert: null,
+    repos: []
   };
 
   // async componentDidMount() {
@@ -45,6 +46,15 @@ class App extends Component {
     this.setState({ user: res.data, loading: false });
   };
 
+  //Get users repos
+  getUserRepos = async username => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?&per_page=5&sort=created:asd&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ repos: res.data, loading: false });
+  };
+
   //Clear the users from the display area after searching
   clearUsers = () => this.setState({ users: [], loading: false });
 
@@ -56,7 +66,7 @@ class App extends Component {
   };
 
   render() {
-    const { users, user, loading } = this.state;
+    const { users, user, loading, repos } = this.state;
 
     return (
       <Router>
@@ -89,7 +99,9 @@ class App extends Component {
                   <User
                     {...props}
                     getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
                     user={user}
+                    repos={repos}
                     loading={loading}
                   />
                 )}
